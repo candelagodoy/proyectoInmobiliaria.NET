a1cf7bc017fa9015cc85ee7023e2b83fbb0a40c9
Candela <candegg13@gmail.com>
2025-08-22 17:57:24 -0300

    Creando proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace proyectoInmobiliaria.NET.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
