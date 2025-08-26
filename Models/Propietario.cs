ce8b793429fa6f1079676d2cac6188708e4d1c4a
Candela <candegg13@gmail.com>
2025-08-25 21:44:58 -0300

    Create Propietario.cs

---
+++ Models/Propietario.cs
@@ -0,0 +1,11 @@
+
+public class Propietario 
+{
+    public string nombre { get; set;}
+    public string apellido { get; set;}
+    public int dni {get; set;}
+    public string direccion {get; set;}
+    public string celular {get; set;}
+    public boolean estado {get; set;}
+    
+}
