46431f1e6f2bdb01af693ff6e6f8c8a07974dac5
FabricioZalazar <zalazar05fabricio@gmail.com>
2025-09-25 00:28:01 -0300

    detallitos

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,10 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using proyectoInmobiliaria.NET.Models;
 
 namespace proyectoInmobiliaria.NET.Controllers;
-
+[Authorize(Policy = "EmpleadoOAdministrador")]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -15,7 +15,6 @@ using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
 
-
 namespace proyectoInmobiliaria.NET.Controllers;
 
 [Authorize]
